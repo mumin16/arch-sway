405a79629c7a528006c0c00f584d25d3297e3133
mumin guler <mumin16@hotmail.com>
2021-10-30 11:39:46 +0300

    Update kur.sh

--- kur.sh
+++ kur.sh
@@ -11,15 +11,15 @@ yay -S --needed --noconfirm pcmanfm lxappearance gvfs ntfs-3g xarchiver rar zip 
 yay -S --needed --noconfirm pulseaudio pulseaudio-alsa alsa-utils
 yay -S --needed --noconfirm chromium
 yay -S --needed --noconfirm networkmanager
-sudo systemctl disable systemd-networkd
-sudo systemctl disable systemd-resolved
-sudo systemctl disable iwd
-sudo systemctl enable NetworkManager
 mkdir ~/.config/sway
 mkdir ~/.config/foot
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/foot.ini --output ~/.config/foot/foot.ini
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/config --output ~/.config/sway/config
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/sway.png --output ~/.config/sway/sway.png
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.bash_profile --output ~/.bash_profile
+sudo systemctl disable systemd-networkd
+sudo systemctl disable systemd-resolved
+sudo systemctl disable iwd
+sudo systemctl enable NetworkManager
 yay -Sc --noconfirm
 reboot
