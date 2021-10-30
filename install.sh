904525ef3ddf2f36453bf306d089800e70f7a2ca
mumin guler <mumin16@hotmail.com>
2021-10-30 17:13:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,12 +13,17 @@ yay -S --needed --noconfirm pulseaudio pulseaudio-alsa alsa-utils
 yay -S --needed --noconfirm chromium
 mkdir ~/.config/sway
 mkdir ~/.config/foot
+echo "export MOZ_ENABLE_WAYLAND=1" >> /etc/profile
+echo "export XDG_CURRENT_DESKTOP=sway" >> /etc/profile
+echo "export XDG_SESSION_TYPE=wayland" >> /etc/profile
+echo "export WLR_NO_HARDWARE_CURSORS=1" >> /etc/profile
+echo "export WLR_RENDERER_ALLOW_SOFTWARE=1" >> /etc/profile
+echo "export _JAVA_AWT_WM_NONREPARENTING=1" >> /etc/profile
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/foot.ini --output ~/.config/foot/foot.ini
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/config --output ~/.config/sway/config
 echo "input type:keyboard xkb_layout" "$(localectl status | grep "X11 Layout" | sed -e "s/^.*X11 Layout://")" >> ~/.config/sway/config
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/sway.png --output ~/.config/sway/sway.png
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.bash_profile --output ~/.bash_profile
-curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.profile --output ~/.profile
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/electron-flags.conf --output ~/.config/electron-flags.conf
 sudo curl https://raw.githubusercontent.com/mumin16/arch-sway/main/10-udisks2.rules --output /etc/polkit-1/rules.d/10-udisks2.rules
 sudo usermod -a -G storage "$USER" 
