aaba77fa118010b6c24a4e15bcb544a754e67110
mumin guler <mumin16@hotmail.com>
2021-10-30 12:39:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ curl https://raw.githubusercontent.com/mumin16/arch-sway/main/sway.png --output 
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.bash_profile --output ~/.bash_profile
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/electron-flags.conf --output ~/.config/electron-flags.conf
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/10-udisks2.rules --output /etc/polkit-1/rules.d/10-udisks2.rules
+sudo usermod -a -G storage "$USER" 
 sudo systemctl disable systemd-networkd
 sudo systemctl disable systemd-resolved
 sudo systemctl disable iwd
