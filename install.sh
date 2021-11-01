23a323a4097ed6860fe2c2fb79c100801e2cd944
mumin guler <mumin16@hotmail.com>
2021-11-01 07:02:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ cd ..
 rm -rf yay-bin
 yay -S --needed --noconfirm nano htop neofetch
 yay -S --needed --noconfirm sway-git foot-git dmenu
-yay -S --needed --noconfirm adwaita-dark
+#yay -S --needed --noconfirm adwaita-dark
 yay -S --needed --noconfirm pcmanfm lxappearance gvfs ntfs-3g xarchiver rar zip unzip 
-curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.gtkrc-2.0 --output ~/.gtkrc-2.0
-yay -S --needed --noconfirm pipewire pipewire-alsa alsa-utils
+#curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.gtkrc-2.0 --output ~/.gtkrc-2.0
+yay -S --needed --noconfirm pulseaudio pulseaudio-alsa alsa-utils
 yay -S --needed --noconfirm chromium
 yay -S --needed --noconfirm noto-fonts-emoji
 mkdir ~/.config/sway
@@ -27,7 +27,7 @@ curl https://raw.githubusercontent.com/mumin16/arch-sway/main/foot.ini --output 
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/config --output ~/.config/sway/config
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/config --output ~/.config/sway/sway_bar.sh
 echo "input type:keyboard xkb_layout" "$(localectl status | grep "X11 Layout" | sed -e "s/^.*X11 Layout://")" >> ~/.config/sway/config
-curl https://raw.githubusercontent.com/mumin16/arch-sway/main/sway.png --output ~/.config/sway/sway.png
+#curl https://raw.githubusercontent.com/mumin16/arch-sway/main/sway.png --output ~/.config/sway/sway.png
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.bash_profile --output ~/.bash_profile
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/electron-flags.conf --output ~/.config/electron-flags.conf
 sudo curl https://raw.githubusercontent.com/mumin16/arch-sway/main/10-udisks2.rules --output /etc/polkit-1/rules.d/10-udisks2.rules
