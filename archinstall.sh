79ec2d13d750b80bc55c3e05330cbb2c33199fe8
mumin guler <mumin16@hotmail.com>
2021-11-03 20:08:32 +0300

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,10 +1,7 @@
 :'
-eğer bağlantınız wifi ise
-
+Eğer bağlantınız wifi ise
 iwlist scanning
 iwctl station wlan0 connect wifi_adi
-
-wifi_şifresi girersiniz istediğinde
 '
 DISK=sda
 PASS=1
@@ -14,44 +11,3 @@ wipefs -a /dev/"$DISK"
 
 mkfs.fat -F32 /dev/"$DISK"1     
 mkfs.ext4 /dev/"$DISK"2
-
-:'
-mount /dev/"$DISK"2 /mnt
-pacstrap /mnt base linux linux-firmware
-
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-timedatectl set-timezone "Europe/Istanbul"
-timedatectl set-ntp true
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "KEYMAP=tr" > /etc/vconsole.conf
-echo "linux" > /etc/hostname
-mkinitcpio -P
-passwd
-echo "$PASS"
-echo "$PASS"
-
-useradd -m x64 -p "$PASS"
-usermod -aG wheel x64
-pacman -S --needed --noconfirm sudo
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-
-pacman -S --needed --noconfirm grub 
-grub-install --target=i386-pc /dev/"$DISK"
-grub-mkconfig -o /boot/grub/grub.cfg
-
-
-pacman -S --needed --noconfirm networkmanager
-systmectl enable NetworkManager
-
-umount -R /mnt
-exit
-reboot
-'
-
-
-
-
-
-
