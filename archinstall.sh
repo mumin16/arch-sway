eece41cbb9e1e43085dd610f6b09d072a5e53cd8
mumin guler <mumin16@hotmail.com>
2021-11-03 22:35:42 +0300

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -7,7 +7,7 @@ DISK=sda
 PASS=1
 
 wipefs -a -f /dev/"$DISK"
-echo -e "n\n\n\n+512m\nn\n\n\n\nw" | fdisk /dev/"$DISK"
+echo -e "n\n\n\n\n+512m\ny\nn\n\n\n\n\nw\n" | fdisk /dev/"$DISK"
 
 mkfs.vfat -F32 /dev/"$DISK"1     
 mkfs.ext4 /dev/"$DISK"2
