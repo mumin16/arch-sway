f28f1a62652e5dbd32002668765356ed3ea29bfa
mumin guler <mumin16@hotmail.com>
2021-11-03 18:28:08 +0300

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,8 +1,10 @@
 :'
-#eğer bağlantınız wifi ise
+eğer bağlantınız wifi ise
+
 iwlist scanning
 iwctl station wlan0 connect wifi_adi
-#wifi_şifresi girersiniz istediğinde
+
+wifi_şifresi girersiniz istediğinde
 '
 DISK=sda
 PASS=1
@@ -31,11 +33,9 @@ mkfs.fat -F32 /dev/"$DISK"1
 mkfs.ext4 /dev/"$DISK"2
 
 :'
-#root u mount et ve temel sistem yüklemelerini yap
 mount /dev/"$DISK"2 /mnt
 pacstrap /mnt base linux linux-firmware
 
-#sistem ayarlamaları
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 timedatectl set-timezone "Europe/Istanbul"
@@ -45,20 +45,20 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=tr" > /etc/vconsole.conf
 echo "linux" > /etc/hostname
 mkinitcpio -P
-passwd #root şifre
-"$PASS"
-"$PASS"
-# kullanici ekle
+passwd
+echo "$PASS"
+echo "$PASS"
+
 useradd -m x64 -p "$PASS"
 usermod -aG wheel x64
 pacman -S --needed --noconfirm sudo
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-#boot yükleyici
+
 pacman -S --needed --noconfirm grub 
 grub-install --target=i386-pc /dev/"$DISK"
 grub-mkconfig -o /boot/grub/grub.cfg
 
-#network
+
 pacman -S --needed --noconfirm networkmanager
 systmectl enable NetworkManager
 
