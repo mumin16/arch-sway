75f6079eab91444a2a826b9587e81cb5546516d9
mumin guler <mumin16@hotmail.com>
2021-11-06 22:18:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay-bin
 yay -S --needed --noconfirm nano htop neofetch
-yay -S --needed --noconfirm sway-git foot-git dmenu noto-fonts-emoji
-yay -S --needed --noconfirm pcmanfm lxappearance gvfs ntfs-3g xarchiver rar zip unzip adwaita-dark
+yay -S --needed --noconfirm sway-git foot-git fuzzel swayidle noto-fonts-emoji
+yay -S --needed --noconfirm pcmanfm lxappearance xarchiver rar zip unzip adwaita-dark
+yay -S --needed --noconfirm gvfs ntfs-3g 
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.gtkrc-2.0 --output ~/.gtkrc-2.0
 #sudo curl https://raw.githubusercontent.com/mumin16/arch-sway/main/10-udisks2.rules --output /etc/polkit-1/rules.d/10-udisks2.rules
 #sudo usermod -a -G storage "$USER" 
