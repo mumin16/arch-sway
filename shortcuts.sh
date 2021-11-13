7a1274ad6c86418d87673344b3d0320147964415
mumin guler <mumin16@hotmail.com>
2021-11-13 07:30:36 +0300

    Add files via upload

---
+++ shortcuts.sh
@@ -0,0 +1,24 @@
+cat << EOF
+ShortCuts:
+
+  = Super
+
+Super+C => Chromium web browser
+Super+D => Directory/file manager
+Super+P => Program launcher
+Super+T => Terminal
+
+Print => ScreenShot
+
+Super+Q => Quit window
+Super+F => Fullscreen window
+Super+arrowkeys => switch window
+Super+leftmouseclicked => replace window in layout
+
+Super+1-2-3-n => switch workspace
+Super+shift+1-2-3-n => send window to other workspace
+
+See More ~/.config/sway/config
+
+EOF
+read -n 1 -r -s
