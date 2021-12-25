8d888e45b7df3c4cba7c4064bdd2576c332724bb
mumin guler <mumin16@hotmail.com>
2021-12-25 15:29:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,15 +26,15 @@ sudo curl https://raw.githubusercontent.com/mumin16/arch-sway/main/electron-flag
 yay -S --needed --noconfirm sddm
 sudo systemctl enable sddm
 
-sudo cp /etc/profile .
-sudo echo "export MOZ_ENABLE_WAYLAND=1" >> profile
-sudo echo "export XDG_CURRENT_DESKTOP=sway" >> profile
-sudo echo "export XDG_SESSION_TYPE=wayland" >> profile
-sudo echo "export WLR_NO_HARDWARE_CURSORS=1" >> profile
-sudo echo "export WLR_RENDERER_ALLOW_SOFTWARE=1" >> profile
-sudo echo "export _JAVA_AWT_WM_NONREPARENTING=1" >> profile
-sudo echo "export QT_QPA_PLATFORMTHEME="qt5ct"" >> profile
-sudo echo "export QT_QPA_PLATFORM=wayland" >> profile
+cp /etc/profile .
+echo "export MOZ_ENABLE_WAYLAND=1" >> profile
+echo "export XDG_CURRENT_DESKTOP=sway" >> profile
+echo "export XDG_SESSION_TYPE=wayland" >> profile
+echo "export WLR_NO_HARDWARE_CURSORS=1" >> profile
+echo "export WLR_RENDERER_ALLOW_SOFTWARE=1" >> profile
+echo "export _JAVA_AWT_WM_NONREPARENTING=1" >> profile
+echo "export QT_QPA_PLATFORMTHEME="qt5ct"" >> profile
+echo "export QT_QPA_PLATFORM=wayland" >> profile
 sudo cp profile /etc/
 rm -rf profile
 
