af4f0fcc1eda77f5af3d6e4114cbf36a8c133dec
mumin guler <mumin16@hotmail.com>
2021-12-25 13:39:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,8 @@ cd ..
 rm -rf yay-bin
 yay -S --needed --noconfirm nano htop neofetch
 yay -S --needed --noconfirm sway-git foot-git rofi swayidle ttf-font-awesome
-yay -S --needed --noconfirm pcmanfm lxappearance xarchiver rar zip unzip material-black-colors-theme
+yay -S --needed --noconfirm mpv qt5-wayland pcmanfm-qt leafpad qt5ct lxqt-archiver rar zip unzip
 yay -S --needed --noconfirm gvfs ntfs-3g 
-curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.gtkrc-2.0 --output ~/.gtkrc-2.0
 yay -S --needed --noconfirm acpi brightnessctl grim
 yay -S --needed --noconfirm pulseaudio pulseaudio-alsa alsa-utils
 mkdir ~/.config/sway
@@ -20,6 +19,8 @@ echo "export XDG_SESSION_TYPE=wayland" >> profile
 echo "export WLR_NO_HARDWARE_CURSORS=1" >> profile
 echo "export WLR_RENDERER_ALLOW_SOFTWARE=1" >> profile
 echo "export _JAVA_AWT_WM_NONREPARENTING=1" >> profile
+echo "export QT_QPA_PLATFORMTHEME="qt5ct"" >> profile
+echo "export QT_QPA_PLATFORM=wayland" >> profile
 sudo cp profile /etc/
 rm -rf profile
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/foot.ini --output ~/.config/foot/foot.ini
@@ -32,13 +33,13 @@ sudo chmod 777 ~/.config/sway/click_time.sh
 sudo chmod 777 ~/.config/sway/swaybar.sh
 echo "input type:keyboard xkb_layout" "$(localectl status | grep "X11 Layout" | sed -e "s/^.*X11 Layout://")" >> ~/.config/sway/config
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/sway.jpg --output ~/.config/sway/sway.jpg
-curl https://raw.githubusercontent.com/mumin16/arch-sway/main/.bash_profile --output ~/.bash_profile
 curl https://raw.githubusercontent.com/mumin16/arch-sway/main/electron-flags.conf --output ~/.config/electron-flags.conf
-curl https://raw.githubusercontent.com/mumin16/arch-sway/main/chromium-flags.conf --output ~/.config/chromium-flags.conf
+yay -S --needed --noconfirm sddm
+sudo systemctl enable sddm
 
 yay -S --needed --noconfirm chromium
 #yay -S --needed --noconfirm libreoffice-dev-bin
-#yay -S --needed --noconfirm obs-studio qt5-wayland mpv
+#yay -S --needed --noconfirm obs-studio qt5-wayland 
 #yay -S --needed --noconfirm visual-studio-code-bin gdb ninja cmake npm
 #yay -S --needed --noconfirm ufw nginx certbot-nginx
 : '
