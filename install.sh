49d7447a7c14eb29c94d5f65522ce5888abd557c
mumin guler <mumin16@hotmail.com>
2022-07-03 12:13:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay-bin
 yay -S --needed --noconfirm nano htop neofetch mpv featherpad
-yay -S --needed --noconfirm sway foot rofi swayidlet ttf-font-awesome
+yay -S --needed --noconfirm sway foot rofi swayidlet ttf-font-awesome oxygen-icons
 yay -S --needed --noconfirm qt5-wayland qt6-wayland xorg-xwayland pcmanfm-qt qt5ct lxqt-policykit lxqt-archiver rar zip unzip
 yay -S --needed --noconfirm gvfs ntfs-3g 
 yay -S --needed --noconfirm acpi brightnessctl grim
