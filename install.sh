de4b7390d94972706977e2de35a91a9d08ceb28e
mumin guler <mumin16@hotmail.com>
2022-07-12 22:02:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ yay -S --needed --noconfirm nano htop neofetch mpv
 yay -S --needed --noconfirm sway swayidle swaybg rofi ttf-font-awesome qt5-wayland qt6-wayland xorg-xwayland qt5ct oxygen-icons 
 yay -S --needed --noconfirm pcmanfm-qt lxqt-policykit lxqt-archiver  qterminal pavucontrol-qt featherpad 
 #yay -S --needed --noconfirm dolphin kate ark konsole polkit-kde-agent
-yay -S --needed --noconfirm rar zip unzip gvfs ntfs-3g 
+yay -S --needed --noconfirm rar zip unzip gvfs ntfs-3g mtpfs gvfs-mtp gvfs-gphoto2
 yay -S --needed --noconfirm acpi brightnessctl grim alsa-utils
 #yay -S --needed --noconfirm pipewire pipewire-media-session
 mkdir ~/.config/sway
